031f47cc617508e094ed00793d7953eb0b4e6bb3
cscroggins <cscroggins@topeka.org>
2016-04-25 10:17:32 -0500

    Updated exercise_4.js

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -26,7 +26,7 @@ var featureLayer = L.mapbox.featureLayer()
 featureLayer.on('ready', function(){
     this.setStyle({
         "color": "#6583BF",
-        "fillColor": "red",
+        "fillColor": "#999",
         "weight": .5,
         "opacity": 0.65
     });
