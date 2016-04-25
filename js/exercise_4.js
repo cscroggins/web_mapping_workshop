5f7ab81bcdb47efa67c92c946827990fbcf785c9
cscroggins <cscroggins@topeka.org>
2016-04-25 09:08:03 -0500

    Updated exercise_4.js

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -2,10 +2,10 @@
 
 // Enter your mapbox map id here to reference it for the base layer,
 // this one references the ugly green map that I made.
-var mapId = 'csccscroggins.pp97ee0e';
+var mapId = 'cscroggins.pp97ee0e';
 
 // And this is my access token, use yours.
-var accessToken = 'pk.epk.eyJ1IjoiY3Njcm9nZ2lucyIsImEiOiJjaW5nMGxwbnIxOXl3dXNrdnZzbmNsczFqIn0.Uo0GFJh7fGIeHZGXSEAX0Q';
+var accessToken = 'pk.eyJ1IjoiY3Njcm9nZ2lucyIsImEiOiJjaW5nMGxwbnIxOXl3dXNrdnZzbmNsczFqIn0.Uo0GFJh7fGIeHZGXSEAX0Q';
 
 // Create the map object with your mapId and token, 
 // referencing the DOM element where you want the map to go.
